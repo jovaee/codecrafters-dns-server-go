2b082b712818a05550abe40fd696b9f454adcf68
jovaee <vaneedenj94@gmail.com>
2025-02-22 15:47:07 +0200

    Question Section

--- app/main.go
+++ app/main.go
@@ -4,13 +4,15 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
+	"strings"
 )
 
 // | ID (16 bits) | QR (1 bit) | OpCode (4 bits) | AA (1 bit) | TC (1 bit) | RD (1 bit) | RA (1 bit) | Z (3 bits) | RCode (4 bits) |
 // |---0---|---1--|-----------------------------2---------------------------------------|------------------3-----------------------|
 
 type DNSMessage struct {
-	Data []byte
+	Data      []byte
+	labelsEnd int
 }
 
 func New(d []byte) DNSMessage {
@@ -23,20 +25,96 @@ func (m *DNSMessage) GetID() uint16 {
 	return binary.BigEndian.Uint16(m.Data[:2])
 }
 
-func (d *DNSMessage) SetID(v uint16) {
-	binary.BigEndian.PutUint16(d.Data[:2], v)
+func (m *DNSMessage) SetID(v uint16) {
+	binary.BigEndian.PutUint16(m.Data[:2], v)
 }
 
-func (d *DNSMessage) GetQR() uint8 {
-	return (d.Data[2] & 0x80) >> 7
+func (m *DNSMessage) GetQR() uint8 {
+	return (m.Data[2] & 0x80) >> 7
 }
 
-func (d *DNSMessage) SetQR(v bool) {
+func (m *DNSMessage) SetQR(v bool) {
 	if v {
-		d.Data[2] |= 0x80
+		m.Data[2] |= 0x80
 	} else {
-		d.Data[2] &= 0x7F
+		m.Data[2] &= 0x7F
 	}
+}
+
+func (m *DNSMessage) GetQDCount() uint16 {
+	return binary.BigEndian.Uint16(m.Data[4:6])
+}
+
+func (m *DNSMessage) SetQDCount(v uint16) {
+	binary.BigEndian.PutUint16(m.Data[4:6], v)
+}
+
+func (m *DNSMessage) GetANCount() uint16 {
+	return binary.BigEndian.Uint16(m.Data[6:8])
+}
+
+func (m *DNSMessage) SetANCount(v uint16) {
+	binary.BigEndian.PutUint16(m.Data[6:8], v)
+}
+
+func (m *DNSMessage) GetQName() string {
+	var sb strings.Builder
+
+	p := 12
+	for m.Data[p] != 0x00 {
+		l := int(m.Data[p]) // How many bytes in this label?
+		p += 1              // Move on from length byte
+
+		// Get each char in the label
+		for i := 0; i < l; i++ {
+			sb.WriteByte(m.Data[p])
+			p += 1
+		}
+
+		if m.Data[p] != 0x00 {
+			sb.WriteByte('.')
+		}
+	}
+
+	return sb.String()
+}
+
+func (m *DNSMessage) SetQName(v string) {
+	sts := strings.Split(v, ".")
+
+	p := 12
+	for _, st := range sts {
+		// Set length of label
+		m.Data[p] = byte(len(st))
+		p += 1
+
+		// Write each of the chars for the label
+		for _, c := range st {
+			m.Data[p] = byte(c)
+			p += 1
+		}
+	}
+
+	m.Data[p] = 0x00    // Null terminator
+	m.labelsEnd = p + 1 // Move past the null terminator
+}
+
+func (m *DNSMessage) GetQType() uint16 {
+	return binary.BigEndian.Uint16(m.Data[m.labelsEnd : m.labelsEnd+2])
+
+}
+
+func (m *DNSMessage) SetQType(v uint16) {
+	binary.BigEndian.PutUint16(m.Data[m.labelsEnd:m.labelsEnd+2], v)
+}
+
+func (m *DNSMessage) GetQClass() uint16 {
+	return binary.BigEndian.Uint16(m.Data[m.labelsEnd+2 : m.labelsEnd+4])
+
+}
+
+func (m *DNSMessage) SetQClass(v uint16) {
+	binary.BigEndian.PutUint16(m.Data[m.labelsEnd+2:m.labelsEnd+4], v)
 }
 
 func main() {
@@ -65,22 +143,31 @@ func main() {
 			break
 		}
 
-		msg := New(buf[:size])
+		request := New(buf[:size])
+		response := New(make([]byte, 512))
 
-		msg.SetID(1234)
-		msg.SetQR(true)
+		response.SetID(request.GetID()) // Response packets must reply with the same ID.
+		response.SetQR(true)            // Indicates it's a response
+		response.SetQDCount(1)          // Number of questions in the query
+		response.SetANCount(1)          // Number of answers in the response
 
-		// fmt.Printf("Byte:  %08b\n", msg.Data[2])
-		// fmt.Printf("ID: %v\n", msg.GetID())
-		// fmt.Printf("QR: %v\n", msg.GetQR())
+		response.SetQName(request.GetQName()) // Domain name
+		response.SetQType(1)                  // A
+		response.SetQClass(1)
 
-		// receivedData := string(buf[:size])
-		// fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+		fmt.Printf("RedID: %v\n", request.GetID())
+		fmt.Printf("ResID: %v\n", response.GetID())
+		fmt.Printf("QR: %v\n", response.GetQR())
+		fmt.Printf("QDCount: %v\n", response.GetQDCount())
+		fmt.Printf("ANCount: %v\n", response.GetANCount())
+		fmt.Printf("QName: %v\n", response.GetQName())
+		fmt.Printf("QType: %v\n", response.GetQType())
+		fmt.Printf("QClass: %v\n", response.GetQClass())
+		fmt.Printf("Request Data: %v\n", request.Data[:size])
+		fmt.Printf("Response Data: %v\n", response.Data[:50])
+		fmt.Printf("=========\n")
 
-		// Create an empty response
-		response := msg.Data
-
-		_, err = udpConn.WriteToUDP(response, source)
+		_, err = udpConn.WriteToUDP(response.Data, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
